f5bbc67ab82829dd7af698c82466bb7535636833
Mateusg2022 <mateusaugustoo100@gmail.com>
2026-01-11 20:55:40 -0300

    usando min priority queue para selecionar proximo nó

--- dijkstra.py
+++ dijkstra.py
@@ -1,52 +1,43 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import heapq
 
-def dijkstra(start_node = 0, end_node = 14):
+def dijkstra(start_node = 0, end_node = 19):
     dist = {node: 1e8 for node in G.nodes()}
     dist[start_node] = 0
 
-    for n, nbrs in G.adj.items():
-        ordered_nbrs= list(nbrs).sort()
-        print(ordered_nbrs)
-        curr_dist = dist[n]
-        for nbr, eattr in nbrs.items(): #itera nos vizinhos do nó atual
-            if(dist[nbr] > (curr_dist + eattr['weight'])):
-                dist[nbr] = curr_dist + eattr['weight']
-    return best_path(dist, start_node, end_node)
+    pq = [(0, start_node)]
+    parents = {node: None for node in G.nodes()}
+    # for n, nbrs in G.adj.items():
+    while pq:
+        curr_dist, curr = heapq.heappop(pq)
 
-def find_closest_nbr(dist, node, previous):
-    curr_dist = 1e7
-    curr_idx = 0
-    for nbr, eattr in G[node].items():
-        if(dist[nbr] < curr_dist and previous != nbr):
-            curr_dist = dist[nbr]
-            curr_idx = nbr
-    # print(curr_idx)
-    return curr_idx
+        if curr == end_node: break
+        if curr_dist > dist[curr]: continue
 
-def best_path(dist, start, end):
+        for nbr, eattr in G[curr].items(): #itera nos vizinhos do nó atual
+            weight = eattr['weight']
+            if(dist[nbr] > (dist[curr] + weight)):
+                dist[nbr] = dist[curr] + weight
+                heapq.heappush(pq, (dist[nbr], nbr))
+                parents[nbr] = curr
     path = []
-    pnode = end
-    path.append(pnode)
-
-    while(pnode != start):
-        previous = pnode
-        pnode = find_closest_nbr(dist, pnode, previous)
-        path.append(pnode)
-    path.reverse()
-    return path
+    curr = end_node
+    while curr is not None:
+        path.append(curr)
+        curr = parents[curr]
+    return path[::-1]
 
 G = nx.generalized_petersen_graph(10, 2)
 
 for u, v in G.edges():
     G[u][v]['weight'] = random.randint(1, 100)
 
-best_path = dijkstra()
-print(best_path)
-node_colors = ["blue" for _ in range(len(G.nodes()))]
+path = dijkstra(0, 4)
+node_colors = ["lightblue" for _ in range(len(G.nodes()))]
 node_list = list(G.nodes())
-for i in best_path:
+for i in path:
     pos_i = node_list.index(i) # isso é feito pois G.nodes pode estar com o numero do nó e seu index desincronizados
     node_colors[pos_i] = "red"
 
